6d325a6e6f2236d696fa489eb9f30b2354c71e91
Thinh Nguyen <gthinh.nguyen@gmail.com>
2021-12-21 17:51:58 -0500

    configured typeorm for app module and added migration scripts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
